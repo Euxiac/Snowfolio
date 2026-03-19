a0ac89847b0878bd8ad9490609d4269cf217f6b3
Christie Tanjaya <christie.tanc@gmail.com>
2026-03-19 18:27:03 +0800

    redeployment with updated url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://caixue.github.io"
+  base: "https://euxiac.github.io/Snowfolio/"
 })
